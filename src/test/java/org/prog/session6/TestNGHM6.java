df4eeb7248de22b9e28d68528df71bef90ea124a
D1Katsiaryna1D <kateonerolling@gmail.com>
2026-07-07 22:45:55 +0200

    HomeWork6

---
+++ src/test/java/org/prog/session6/TestNGHM6.java
@@ -0,0 +1,41 @@
+package org.prog.session6;
+
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+public class TestNGHM6 {
+
+    @Test
+    public void HM6(){
+
+        // tax id, each tax id can have 1-5 FOPs
+        Map<String, List<String>> map = new HashMap<>();
+        map.put("17253761525", new ArrayList<>());
+
+        map.get("17253761525").add("FOP #1");
+        map.get("17253761525").add("FOP #2");
+        map.get("17253761525").add("FOP #3");
+        map.get("17253761525").add("FOP #4");
+
+        List<String> list1 = map.get("17253761525");
+
+        for (String someString : list1) {
+            System.out.println("Citizen 17253761525 has : " + someString);
+        }
+    }
+
+    public static String randomColor() {
+        Random random = new Random();
+        return switch (random.nextInt(5)) {
+            case 0 -> "red";
+            case 1 -> "green";
+            case 2 -> "blue";
+            case 3 -> "yellow";
+            case 4 -> "pink";
+            default -> "black";
+        };
+    }
+}
+
